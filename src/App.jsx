2c13bd681702885e14c1363674bd33b74249aa3e
rakhelmoryio <168238218+rakhelmoryio@users.noreply.github.com>
2024-05-02 13:50:48 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,13 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import BookingForm from "./pages/BookingForm";
 import BookingData from "./pages/BookingData";
+import BookingUpdate from "./pages/BookingUpdate";
 const App = () => {
   return (
     <>
       <BrowserRouter>
         <Routes>
+          <Route path="/update" element={<BookingUpdate/>} />
           <Route path="/booking" element={<BookingForm/>} />
           <Route path="/" element={<BookingData/>} />
         </Routes>
